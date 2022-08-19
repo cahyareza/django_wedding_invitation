2643dbe73c9a95c6155cd4ee11720f3297153c18
cahyafaisalreza <cf.reza07@gmail.com>
2022-08-19 14:19:32 +0700

    setting locale to id, timezone jakarta, and delete prefix.

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,4 +22,5 @@ from django.views.generic import TemplateView
 urlpatterns = i18n_patterns(
     path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
+    prefix_default_language=False,
 )
