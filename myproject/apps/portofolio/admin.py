a2565d7ac935b94514b5f1f75c401c5bacf43a66
cahyafaisalreza <cf.reza07@gmail.com>
2022-08-28 23:20:10 +0700

    Change_porto_model

--- myproject/apps/portofolio/admin.py
+++ myproject/apps/portofolio/admin.py
@@ -2,7 +2,42 @@ from django.contrib import admin
 from .models import (Couple, Acara, Ourmoment, SpecialInvitation, Ucapan,
      Quotes, AddtoCalender, Goto, Hadir, Dompet, Portofolio)
 
+#Inline
+class AcaraInline(admin.TabularInline):
+    model = Acara
+
+class CoupleInline(admin.TabularInline):
+    model = Couple
+
+class OurmomentInline(admin.TabularInline):
+    model = Ourmoment
+
+class SpecialInvitationInline(admin.TabularInline):
+    model = SpecialInvitation
+
+class UcapanInline(admin.TabularInline):
+    model = Ucapan
+
+class QuotesInline(admin.TabularInline):
+    model = Quotes
+
+class AddtoCalenderInline(admin.TabularInline):
+    model = AddtoCalender
+
+class GotoInline(admin.TabularInline):
+    model = Goto
+
+class HadirInline(admin.TabularInline):
+    model = Hadir
+
+class DompetInline(admin.TabularInline):
+    model = Dompet
+
+
+
+
 # Register your models here.
+admin.site.register(Couple)
 admin.site.register(Acara)
 admin.site.register(Ourmoment)
 admin.site.register(SpecialInvitation)
@@ -12,8 +47,9 @@ admin.site.register(AddtoCalender)
 admin.site.register(Goto)
 admin.site.register(Hadir)
 admin.site.register(Dompet)
-admin.site.register(Portofolio)
 
-@admin.register(Couple)
-class CoupleAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Portofolio)
+class PortofolioAdmin(admin.ModelAdmin):
+    inlines = [AcaraInline, CoupleInline, OurmomentInline, SpecialInvitationInline,
+               UcapanInline, QuotesInline, AddtoCalenderInline, GotoInline, HadirInline,
+               DompetInline]
--- myproject/apps/portofolio/models.py
+++ myproject/apps/portofolio/models.py
@@ -4,7 +4,13 @@ from django.utils.translation import gettext_lazy as _
 
 from myproject.apps.core.models import CreationModificationDateBase
 
+class Portofolio(CreationModificationDateBase):
+    name = models.CharField(max_length=250)
+    # sementara belum mengunakan app account karena belum dibuat
+    user = models.CharField(max_length=250)
+
 class Couple (CreationModificationDateBase):
+    portofolio = models.OneToOneField(Portofolio, on_delete=models.CASCADE, blank=True, null=True)
     pname = models.CharField(max_length=40, blank=True, null=True)
     pinsta_link = models.CharField(max_length=250)
     panak_ke = models.IntegerField()
@@ -22,6 +28,7 @@ class Couple (CreationModificationDateBase):
         verbose_name_plural = "Couple"
 
 class Acara(CreationModificationDateBase):
+    portofolio = models.OneToOneField(Portofolio, on_delete=models.CASCADE, blank=True, null=True)
     tanggal_akad = models.DateField()
     tanggal_selesai_akad = models.DateField()
     tempat_akad = models.CharField(max_length=250)
@@ -39,6 +46,7 @@ class Acara(CreationModificationDateBase):
         verbose_name_plural = "Acara"
 
 class Ourmoment(CreationModificationDateBase):
+    portofolio = models.OneToOneField(Portofolio, on_delete=models.SET_NULL, blank=True, null=True)
     video = models.CharField(max_length=250)
     photo = models.CharField(max_length=250)
     livestream = models.CharField(max_length=250)
@@ -47,12 +55,14 @@ class Ourmoment(CreationModificationDateBase):
         verbose_name_plural = "Ourmoment"
 
 class SpecialInvitation(CreationModificationDateBase):
+    portofolio = models.ForeignKey(Portofolio, on_delete=models.SET_NULL, blank=True, null=True)
     name = models.CharField(max_length=40)
 
     class Meta:
         verbose_name_plural = "SpecialInvitation"
 
 class Ucapan(CreationModificationDateBase):
+    portofolio = models.ForeignKey(Portofolio, on_delete=models.SET_NULL, blank=True, null=True)
     name = models.CharField(max_length=40)
     alamat = models.CharField(max_length=40)
     pesan = models.CharField(max_length=60)
@@ -61,6 +71,7 @@ class Ucapan(CreationModificationDateBase):
         verbose_name_plural = "Ucapan"
 
 class Quotes(CreationModificationDateBase):
+    portofolio = models.OneToOneField(Portofolio, on_delete=models.CASCADE, blank=True, null=True)
     ayat = models.CharField(max_length=250)
     kutipan = models.TextField()
 
@@ -68,6 +79,7 @@ class Quotes(CreationModificationDateBase):
         verbose_name_plural = "Quotes"
 
 class AddtoCalender(CreationModificationDateBase):
+    portofolio = models.OneToOneField(Portofolio, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=250)
     startDate = models.DateField()
@@ -83,6 +95,7 @@ class AddtoCalender(CreationModificationDateBase):
         verbose_name_plural = "AddtoCalender"
 
 class Goto(CreationModificationDateBase):
+    portofolio = models.OneToOneField(Portofolio, on_delete=models.CASCADE, blank=True, null=True)
     link_iframe = models.CharField(max_length=250)
     lokasi = models.CharField(max_length=250)
     link_gmap = models.CharField(max_length=250)
@@ -91,6 +104,7 @@ class Goto(CreationModificationDateBase):
         verbose_name_plural = "Goto"
 
 class Hadir(CreationModificationDateBase):
+    portofolio = models.ForeignKey(Portofolio, on_delete=models.SET_NULL, blank=True, null=True)
     name = models.CharField(max_length=40)
     hadir = models.CharField(max_length=40)
 
@@ -98,6 +112,7 @@ class Hadir(CreationModificationDateBase):
         verbose_name_plural = "Hadir"
 
 class Dompet(CreationModificationDateBase):
+    portofolio= models.ForeignKey(Portofolio, on_delete=models.SET_NULL, blank=True, null=True)
     rekening = models.CharField(max_length=40)
     nomor = models.IntegerField()
     pemilik = models.CharField(max_length=40)
@@ -105,21 +120,3 @@ class Dompet(CreationModificationDateBase):
     class Meta:
         verbose_name_plural = "Dompet"
 
-class Portofolio(CreationModificationDateBase):
-    name = models.CharField(max_length=250)
-    # sementara belum mengunakan app account karena belum dibuat
-    user = models.CharField(max_length=250)
-    # one to one relationship
-    couple = models.OneToOneField(Couple, on_delete=models.CASCADE)
-    acara =  models.OneToOneField(Acara, on_delete=models.CASCADE)
-    quotes = models.OneToOneField(Quotes, on_delete=models.CASCADE)
-    addtocalender = models.OneToOneField(AddtoCalender, on_delete=models.CASCADE)
-    goto = models.OneToOneField(Goto, on_delete=models.CASCADE)
-
-    # One to many relationship
-    # Jika kelas gratis maka tidak usah menggunakan fiture ourmoment, sehingga jika didelete maka tetap ada portonya
-    ourmoment = models.OneToOneField(Ourmoment, on_delete=models.SET_NULL, blank=True, null=True)
-    specialinvitation = models.ForeignKey(SpecialInvitation, on_delete=models.SET_NULL, blank=True, null=True)
-    ucapan = models.ForeignKey(Ucapan, on_delete=models.SET_NULL, blank=True, null=True)
-    hadir = models.ForeignKey(Hadir, on_delete=models.SET_NULL, blank=True, null=True)
-    dompet = models.ForeignKey(Dompet, on_delete=models.SET_NULL, blank=True, null=True)
