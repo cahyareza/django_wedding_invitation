c9bc960a5d636be130db46481698fa5901fb329e
cahyafaisalreza <cf.reza07@gmail.com>
2023-01-30 11:55:43 +0700

    -- minor

--- myproject/settings/production.py
+++ myproject/settings/production.py
@@ -2,7 +2,7 @@ from ._base import *
 
 DEBUG = False
 WEBSITE_URL = "https://www.einvitation.net"  # without trailing slash
-WEBSITE_URL_FRONTEND = "https://www.einvitation.net/core/"
+WEBSITE_URL_FRONTEND = "https://www.einvitation.net/open/"
 NAMA_DOMAIN = "www.einvitation.net"
 ALLOWED_HOSTS = [
     "*",
