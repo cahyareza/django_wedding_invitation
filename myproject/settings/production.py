407862bb0972cb1b8560bde8a2668c8f73f83983
cahyafaisalreza <cf.reza07@gmail.com>
2023-02-11 20:26:28 +0700

    -- minor

--- myproject/settings/production.py
+++ myproject/settings/production.py
@@ -4,6 +4,9 @@ DEBUG = False
 WEBSITE_URL = "https://www.einvitation.net"  # without trailing slash
 WEBSITE_URL_FRONTEND = "https://www.einvitation.net/open/"
 NAMA_DOMAIN = "www.einvitation.net"
+CORS_ALLOWED_ORIGINS = [
+    "https://www.einvitation.net",
+]
 SITE_ID = 4
 ALLOWED_HOSTS = [
     "*",
